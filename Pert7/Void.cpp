7de46b4109ec116d182d1e800c16bf12574ddb76
Rafly1818 <raflyravsanjani@gmail.com>
2024-11-25 23:14:37 +0700

    fresh

--- Pert7/Void.cpp
+++ Pert7/Void.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 void luasPersegi(){
     int a, b;
-    cout << "-----Luas Persegi-----";
-    cout << "Rumus Luas Persegi" << endl << "L = s x s";
+    cout << "-----Luas Persegi-----" << endl;
+    cout << "Rumus Luas Persegi" << endl << "L = s x s" << endl;
     cout << "Masukkan Panjang Sisi 1 : ";
     cin >> a;
     cout << "Masukkan Panjang Sisi 2 : ";
     cin >> b;
+    cout << endl << "Jawaban: " << a*b << endl;
     
 }
 
 int main()
 {
-    luasPersegi()
+    luasPersegi();
     
 
     return 0;
